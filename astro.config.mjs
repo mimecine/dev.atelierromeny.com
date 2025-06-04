6a4dcecbc2c69066b34ce4879e72b08cfbc8f907
Marco Romeny <marco@mimecom.net>
2025-06-04 13:42:59 +0200

    Added github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,7 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://dev.atelierromeny.com',
   vite: {
     plugins: [tailwindcss()]
   },
